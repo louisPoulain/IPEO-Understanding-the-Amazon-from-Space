1a24d03cb9cf30e43bff3c4ad795ac54eb83824e
lpoulain <louis.poulain-auzeau@epfl.ch>
2022-11-25 11:24:08 +0100

    model

--- model.py
+++ model.py
@@ -4,16 +4,14 @@ import pytorch_lightning as pl
 
 import torch
 import torch.functional as F
+import torch.nn as nn
 import torchvision
 
 
 class PlanetModel(pl.LightningModule):
-    def __init__(self, pretrained = False):
+    def __init__(self, model = None):
         super().__init__()
-        if pretrained:
-            self.model = torchvision.models.resnet18(num_classes=21) ## TO BE CHANGED
-        else:
-            self.model = None ## TO BE CHANGED
+        self.model = model
 
     def forward(self, x):
         return self.model(x)
@@ -40,3 +38,23 @@ class PlanetModel(pl.LightningModule):
     def configure_optimizers(self):
         return torch.optim.Adam(self.model.parameters(), lr=0.001)
         
+class testModel(nn.Module):
+    def __init__(self, pretrained:bool=False):
+        self.pretrained = pretrained
+        if self.pretrained:
+            self.model = torchvision.models.AlexNet() ## A CHANGER
+        else:
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1) #256
+            self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3) # 126
+            self.maxpool = nn.MaxPool2d(kernel_size=2) # 128
+            self.enc = nn.Seqential(self.conv1, self.maxpool, self.conv2)
+            self.deconv1 = nn.ConvTranspose2d(in_channels=16, out_channels=3, kernel_size=3) # 128
+            self.upsample = nn.Upsample(scale_factor=2) # 256
+            self.dec = nn.Sequential(self.deconv1, self.upsample)
+
+    def forward(self, x):
+        if self.pretrained:
+            x = self.model(x)
+            return x
+        else:
+            None
