050af8c3bf2c1ce58fb591aa0c336bdb478fffd6
lpoulain <louis.poulain-auzeau@epfl.ch>
2022-12-16 10:21:06 +0100

    add npy option

--- dataset.py
+++ dataset.py
@@ -11,19 +11,11 @@ import torchvision.transforms as T
 import time
 import matplotlib.pyplot as plt
 
-#def get_data(path):
-#    if not os.path.exists("/content/drive/MyDrive/Colab_Notebooks/Observation-earth/IPEO_Planet_project.zip"):
-#            print(f"downloading IPEO_Planet_project.zip")
-#            urllib.request.urlretrieve("https://drive.google.com/file/d/1zS6wtrRfTeLaHwlS0HDlNiAc_b_ZrTeX/view?usp=share_link",
-#                                       "/content/drive/MyDrive/Colab_Notebooks/Observation-earth/IPEO_Planet_project.zip")
-#    else:
-#        print(3)
-#    with zipfile.ZipFile(path, 'r') as zip_ref:
-#        zip_ref.extractall()
+
 
 
 class DatasetAmazon(Dataset):
-    def __init__(self, full=False, tiny=False, test=False, val=False, split=[0.6, 0.15, 0.25], path_to_labels=None):
+    def __init__(self, full=False, tiny=False, test=False, val=False, split=[0.6, 0.15, 0.25], path_to_labels=None, read_npy=False):
         """images dataset initialization
 
         Args:
@@ -33,25 +25,29 @@ class DatasetAmazon(Dataset):
             test (bool, optional): load test data. Defaults to False.
             val (bool, optional): load validation data. Defaults to False.
             split (list[floats], optional): how to split the data (the sum needs to be equal to one)
+            read_npy = whether images should be taken from the npy file or not
         """
         self.full = full
         self.tiny = tiny
         self.test = test
         self.val = val
         self.split = split
+        self.read_npy = read_npy
         if not sum(self.split) == 1.0:
             raise ValueError('The splitting procedure needs to equal 1')
         if not len(self.split) == 3:
             raise ValueError('You need to provide a splitting value for the train, val, and test (3 values)')
 
         ###### A FAIRE PLUS INTELLIGEMMENT #####
-        train_stop = int(np.floor(self.split[0]*40479))
-        val_stop = int(np.floor((self.split[0]+self.split[1])*40479))
+        indexes = np.arange(stop=40479)
+        indexes = np.random.shuffle(indexes)
+        train_stop = int(np.floor(self.split[0]*len(indexes)))
+        val_stop = int(np.floor((self.split[0]+self.split[1])*len(indexes)))
         
         self.SPLITS = {
-        'train': np.arange(0, train_stop),    
-        'val':   np.arange(train_stop+1, val_stop),   
-        'test':  np.arange(val_stop+1, 40479)  
+        'train': indexes[:train_stop],    
+        'val':   indexes[train_stop+1, val_stop],   
+        'test':  indexes[val_stop+1, len(indexes)]  
         }
         ########################################
         self.LABEL_CLASSES = pd.read_pickle(path_to_labels)
@@ -59,8 +55,12 @@ class DatasetAmazon(Dataset):
 
     def __getitem__(self, index):
         #t1 = time.time()
-        imgName, label = self.data[index]
-        im = plt.imread(imgName)
+        if self.read_npy:
+            imgName, imgIndex, label = self.data[index]
+            im = np.load(imgName)[:, :, :, imgIndex]
+        else:
+            imgName, label = self.data[index]
+            im = plt.imread(imgName)
         im = im.transpose(2, 0, 1)
         img = torch.from_numpy(np.array(im))
         #print("getitem: ", time.time()-t1)
@@ -77,39 +77,36 @@ class DatasetAmazon(Dataset):
         if self.val:
             print(f"Loading validation data ({self.SPLITS['val'].shape[0]} images)")
             for imgIndex in self.SPLITS['val']:
-                imgName = os.path.join(data_loc, 
-                                f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
-                # example format: 'baseFolder/agricultural/agricultural07.tif'
-                #img = plt.imread(imgName)
-                #img = transform(img)
-                self.data.append((
-                    imgName,
-                    self.LABEL_CLASSES.iloc[imgIndex].values         # get index for label class
-                ))
+                if self.read_npy:
+                    imgName = os.path.join(data_loc,
+                            f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
+                    self.data.append((imgName, imgIndex-imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
+                else:
+                    imgName = os.path.join(data_loc, 
+                            f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
+                    self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
         elif self.test:
             print(f"Loading test data ({self.SPLITS['test'].shape[0]} images)")
             for imgIndex in self.SPLITS['test']:
-                imgName = os.path.join(data_loc, 
-                                f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
-                # example format: 'baseFolder/agricultural/agricultural07.tif'
-                #img = plt.imread(imgName) #img = Image.open(imgName)
-                #img = transform(img)
-                self.data.append((
-                    imgName,
-                    self.LABEL_CLASSES.iloc[imgIndex].values         # get index for label class
-                ))
+                if self.read_npy:
+                    imgName = os.path.join(data_loc,
+                            f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
+                    self.data.append((imgName, imgIndex-imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
+                else:
+                    imgName = os.path.join(data_loc, 
+                            f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
+                    self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
         else:
             print(f"Loading training data ({self.SPLITS['train'].shape[0]} images)")
             for imgIndex in self.SPLITS['train']:
-                imgName = os.path.join(data_loc, 
-                                f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
-                # example format: 'baseFolder/agricultural/agricultural07.tif'
-                #img = plt.imread(imgName) #img = Image.open(imgName)
-                #img = transform(img)
-                self.data.append((
-                    imgName,
-                    self.LABEL_CLASSES.iloc[imgIndex].values         # get index for label class
-                ))
+                if self.read_npy:
+                    imgName = os.path.join(data_loc,
+                            f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
+                    self.data.append((imgName, imgIndex-imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
+                else:
+                    imgName = os.path.join(data_loc, 
+                            f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
+                    self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
         if self.full:
             if self.tiny:
                 raise ValueError('Cannot have both --full and --tiny')
