f30fef8bb1938efcec35909e06a559bb69c068a7
lpoulain <louis.poulain-auzeau@epfl.ch>
2022-12-20 17:52:41 +0100

    chnage dataset fct

--- dataset.py
+++ dataset.py
@@ -38,16 +38,28 @@ class DatasetAmazon(Dataset):
         if not len(self.split) == 3:
             raise ValueError('You need to provide a splitting value for the train, val, and test (3 values)')
 
-        indexes = np.arange(stop=40479)
-        np.random.shuffle(indexes)
-        train_stop = int(np.floor(self.split[0]*len(indexes)))
-        val_stop = int(np.floor((self.split[0]+self.split[1])*len(indexes)))
+        # Separate rare and frequent indexes
+        all_ind = get_all_indexes()
+        labels = ["frequent", "conventional_mine", "bare_ground", "artisinal_mine",
+                 "blooming", "blow_down", "select_logging", "slash_burn"]
+        dict_labels = dict([(labels[i], all_ind[i]) for i in range(7)])          
+
+        # Splitting procedure
+        for label in dict_labels.keys():
+            tmp = dict_labels[label]
+            np.random.shuffle(tmp)
+            l = len(tmp)
+            train_stop = int(np.floor(self.split[0]*l))
+            val_stop = int(np.floor((self.split[0]+self.split[1])*l))
+            SPLITS = {
+                'train': tmp[:train_stop],    
+                'val':   tmp[train_stop+1:val_stop],   
+                'test':  tmp[val_stop+1:l]
+                }
+            dict_labels[label] = SPLITS
         
-        self.SPLITS = {
-        'train': indexes[:train_stop],    
-        'val':   indexes[train_stop+1:val_stop],   
-        'test':  indexes[val_stop+1:len(indexes)]  
-        }
+        self.SPLITS = dict_labels
+        self.all_ind = all_ind
         ########################################
         self.LABEL_CLASSES = pd.read_pickle(path_to_labels)
         self.load_data()
@@ -72,51 +84,53 @@ class DatasetAmazon(Dataset):
         t = time.time()
         data_loc = "../IPEO_Planet_project/"
         self.data = []                                  # list of tuples of (image path, label class)
-        #transform = T.ToTensor()
-        if self.val:
-            print(f"Loading validation data ({self.SPLITS['val'].shape[0]} images)")
-            for imgIndex in self.SPLITS['val']:
-                if self.read_npy:
-                    imgName = os.path.join(data_loc,
-                            f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
-                    self.data.append((imgName, imgIndex-(imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
-                else:
-                    imgName = os.path.join(data_loc, 
-                            f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
-                    self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
-        elif self.test:
-            print(f"Loading test data ({self.SPLITS['test'].shape[0]} images)")
-            for imgIndex in self.SPLITS['test']:
-                if self.read_npy:
-                    imgName = os.path.join(data_loc,
-                            f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
-                    self.data.append((imgName, imgIndex-(imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
-                else:
-                    imgName = os.path.join(data_loc, 
-                            f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
-                    self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
-        else:
-            print(f"Loading training data ({self.SPLITS['train'].shape[0]} images)")
-            for imgIndex in self.SPLITS['train']:
-                if self.read_npy:
-                    imgName = os.path.join(data_loc,
-                            f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
-                    self.data.append((imgName, imgIndex-(imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
-                else:
-                    imgName = os.path.join(data_loc, 
-                            f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
-                    self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
-        if self.full:
-            if self.tiny:
-                raise ValueError('Cannot have both --full and --tiny')
-        else:
-            if self.tiny:
-                print('** Reduce the data-set to the tiny setup (factor of 1000)')
-                self.data = self.data[0:-1:1000]
+        #if self.val:
+        #    nb_img = sum(map(len, self.all_ind))
+        #    print(f"Loading validation data ({self.SPLITS['val'].shape[0]} images)")
+        #print(f"Loading test data ({self.SPLITS['test'].shape[0]} images)")
+        #print(f"Loading training data ({self.SPLITS['train'].shape[0]} images)")
+        for label in self.SPLITS.keys():
+            if self.val:
+                for imgIndex in self.SPLITS[label]['val']:
+                    if self.read_npy:
+                        imgName = os.path.join(data_loc,
+                                f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
+                        self.data.append((imgName, imgIndex-(imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
+                    else:
+                        imgName = os.path.join(data_loc, 
+                                f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
+                        self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
+            elif self.test:
+                for imgIndex in self.SPLITS[label]['test']:
+                    if self.read_npy:
+                        imgName = os.path.join(data_loc,
+                                f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
+                        self.data.append((imgName, imgIndex-(imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
+                    else:
+                        imgName = os.path.join(data_loc, 
+                                f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
+                        self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
             else:
-                print('** Reduce the data-set by a factor 100 (use --full for the full thing)')
-                self.data = self.data[0:-1:100]
-        print("loading took ", time.time()-t)
+                for imgIndex in self.SPLITS[label]['train']:
+                    if self.read_npy:
+                        imgName = os.path.join(data_loc,
+                                f'train-jpg/numpy_ndarray_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}.npy')
+                        self.data.append((imgName, imgIndex-(imgIndex//1000)*1000, self.LABEL_CLASSES.iloc[imgIndex].values))
+                    else:
+                        imgName = os.path.join(data_loc, 
+                                f'train-jpg/train_{(imgIndex//1000)*1000}-{(imgIndex//1000+1)*1000-1}/train_{str(imgIndex)}.jpg') 
+                        self.data.append((imgName, self.LABEL_CLASSES.iloc[imgIndex].values))
+            if self.full:
+                if self.tiny:
+                    raise ValueError('Cannot have both --full and --tiny')
+            else:
+                if self.tiny:
+                    print('** Reduce the data-set to the tiny setup (factor of 1000)')
+                    self.data = self.data[0:-1:1000]
+                else:
+                    print('** Reduce the data-set by a factor 100 (use --full for the full thing)')
+                    self.data = self.data[0:-1:100]
+            print("loading took ", time.time()-t)
 
 
 
